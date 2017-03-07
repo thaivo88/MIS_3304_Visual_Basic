a43644dff38d3f93cde65a4cd0f55290bde06896
thai vo <thaivo88@gmail.com>
2017-03-07 16:43:38 -0600

    Roman Numeral Converter
    created a exe file using visual studio to convert roman numeral from integer 1-10

---
+++ RomanNumeralConverter/RomanNumeralConverter/Form1.vb
@@ -0,0 +1,50 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+        Dim number = TextBox1.Text
+        Dim romanNumeral = TextBox2.Text
+
+        If number < 1 Then
+            romanNumeral = "Value is either out of bound or a non-integer"
+        ElseIf number = 1 Then
+            romanNumeral = "I"
+        ElseIf number = 2 Then
+            romanNumeral = "II"
+        ElseIf number = 3 Then
+            romanNumeral = "III"
+        ElseIf number = 4 Then
+            romanNumeral = "IV"
+        ElseIf number = 5 Then
+            romanNumeral = "V"
+        ElseIf number = 6 Then
+            romanNumeral = "VI"
+        ElseIf number = 7 Then
+            romanNumeral = "VII"
+        ElseIf number = 8 Then
+            romanNumeral = "VIII"
+        ElseIf number = 9 Then
+            romanNumeral = "IX"
+        ElseIf number = 10 Then
+            romanNumeral = "X"
+        ElseIf number > 10 Then
+            romanNumeral = "Value is either out of bound or a non-integer"
+        End If
+
+        MessageBox.Show(romanNumeral)
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+
+        TextBox1.Clear()
+        TextBox2.Clear()
+
+    End Sub
+
+    Private Sub Button3_click(sender As Object, e As EventArgs) Handles Button3.Click
+
+        Application.Exit()
+
+    End Sub
+
+End Class
